561d732346820ecf99caa81a1727276432a48719
haonanu <haonan.wu@ulabsystems.com>
2022-07-12 13:59:54 +0800

    HW

---
+++ HW/HJ5.ts
@@ -0,0 +1,23 @@
+/**
+ * 
+ * 描述
+    写出一个程序，接受一个十六进制的数，输出该数值的十进制表示。
+
+    数据范围：保证结果在 1 <= N <= 2^31-1 
+
+    输入描述：
+    输入一个十六进制的数值字符串。
+
+    输出描述：
+    输出该数值的十进制字符串。不同组的测试用例用\n隔开。
+ */
+import readline from "readline";
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+rl.on("line", function (line) {
+  var num = parseInt(line, 16);
+  console.log(num);
+});
